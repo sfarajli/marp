d807a3dc8fdc4f2e499bcc73d9adc111cb51086f
Farid <faridfaracli9@gmail.com>
2024-12-23 12:50:55 +0400

    add initial compilation support

--- src/main.go
+++ src/main.go
@@ -93,6 +93,80 @@ func interpret(raw[]string) {
 	} 
 }
 
+func compile(raw []string) {
+	var err error;
+	var number int;
+
+	fmt.Println("format ELF64 executable")
+	fmt.Println("")
+	fmt.Println("_start:")
+
+	for i := 0; i < len(raw); i++ {
+		number, err = strconv.Atoi(raw[i]);
+		if err == nil {
+			fmt.Printf("	push %v\n", number)
+			continue; 
+		}
+
+		switch raw[i] {
+		case "+":
+			fmt.Println("	pop r10")
+			fmt.Println("	pop rax")
+			fmt.Println("	add rax, r10")
+			fmt.Println("	push rax")
+		case "-":
+			fmt.Println("	pop r10")
+			fmt.Println("	pop rax")
+			fmt.Println("	sub rax, r10")
+			fmt.Println("	push rax")
+		case ".":
+			fmt.Println("	pop rdi")
+			fmt.Println("	call _dump")
+
+		default:
+			panic("invalid word");
+		}
+	} 
+
+	fmt.Println("")
+	fmt.Println("_end:")
+	fmt.Println("	mov rax, 60")
+	fmt.Println("	mov rdi, 0")
+	fmt.Println("	syscall")
+
+	fmt.Println("")
+	fmt.Println("_dump:")
+	fmt.Println("	mov rax, rdi")
+	fmt.Println("	mov r10, 0")
+	fmt.Println("")
+	fmt.Println("	dec rsp")
+	fmt.Println("	mov byte [rsp], 10")
+	fmt.Println("	inc r10")
+	fmt.Println("")
+	fmt.Println("_prepend_digit:")
+	fmt.Println("	mov rdx, 0")
+	fmt.Println("	mov rbx, 10")
+	fmt.Println("	div rbx")
+	fmt.Println("")
+	fmt.Println("	add rdx, 48")
+	fmt.Println("	dec rsp")
+	fmt.Println("	mov [rsp], dl")
+	fmt.Println("	inc r10")
+	fmt.Println("")
+	fmt.Println("	cmp rax, 0")
+	fmt.Println("	jne _prepend_digit")
+	fmt.Println("")
+	fmt.Println("_print_digit:")
+	fmt.Println("	mov rax, 1")
+	fmt.Println("	mov rdi, 1")
+	fmt.Println("	mov rsi, rsp")
+	fmt.Println("	mov rdx, r10")
+	fmt.Println("	syscall")
+	fmt.Println("")
+	fmt.Println("	add rsp, r10")
+	fmt.Println("	ret")
+}
+
 func main() {
 	var file string;
 	argv := os.Args;
@@ -114,7 +188,7 @@ func main() {
 
 	output := readfile(file);
 	if (compile_flg) {
-		panic("Not Implemented");
+		compile(output);
 	} else {
 		interpret(output);
 	}
